c373899a382f438655caef0037e8f28184234ade
Teran McKinney <sega01@go-beyond.org>
2017-12-20 22:04:56 +0000

    0.4.2: Raise exception when using pay2sh addresses
    Comes from: https://github.com/ofek/bit/pull/24

--- bitcash/__init__.py
+++ bitcash/__init__.py
@@ -4,4 +4,4 @@ from bitcash.network.rates import SUPPORTED_CURRENCIES, set_rate_cache_time
 from bitcash.network.services import set_service_timeout
 from bitcash.wallet import Key, PrivateKey, PrivateKeyTestnet, wif_to_key
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
