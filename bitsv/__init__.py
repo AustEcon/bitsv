958f29953654b4b30de73e6065af2802cbd565bc
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:39:39 -0700

    Fixed PYTHON_F401 issues in bitsv/__init__.py

--- bitsv/__init__.py
+++ bitsv/__init__.py
@@ -1,7 +1,2 @@
-from bitsv.format import verify_sig
-from bitsv.network.rates import SUPPORTED_CURRENCIES, set_rate_cache_time
-from bitsv.network.services import set_service_timeout
-from bitsv.wallet import Key, PrivateKey, PrivateKeyTestnet, wif_to_key
-from bitsv.bip32utils import Bip32utils
 
 __version__ = '0.6.0'
