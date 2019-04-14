5ee0cf4c1282a5bc91c51a2534fa5c41e2a04bd1
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 22:39:45 -0700

    Fixed PYTHON_F401 issues in bitsv/network/__init__.py

--- bitsv/network/__init__.py
+++ bitsv/network/__init__.py
@@ -1,6 +1,4 @@
-from .fees import get_fee
 from .rates import (
     currency_to_satoshi, currency_to_satoshi_cached,
     satoshi_to_currency, satoshi_to_currency_cached
 )
-from .services import NetworkAPI
