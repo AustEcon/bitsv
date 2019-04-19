39e11c28fba0d6e3cd4e244d701e772cd2f4c924
cyborg0922 <cyborg.napster.0922@gmail.com>
2019-04-19 23:33:07 +1200

    version 0.7.1

--- bitsv/__init__.py
+++ bitsv/__init__.py
@@ -4,4 +4,4 @@ from bitsv.network.services import set_service_timeout
 from bitsv.wallet import Key, PrivateKey, PrivateKeyTestnet, wif_to_key
 from bitsv.bip32 import Bip32, Bip32utils
 
-__version__ = '0.6.1'
+__version__ = '0.7.1'
