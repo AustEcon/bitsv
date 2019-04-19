9888043aa7585404a41f547a8be99b09d27f0910
cyborg0922 <cyborg.napster.0922@gmail.com>
2019-04-19 21:10:13 +1200

    Add functions to bip32: get_xprv, private_key, get_children, sign, verify, chain_code, fingerprint

--- bitsv/bip32.py
+++ bitsv/bip32.py
@@ -1,5 +1,3 @@
-import pycoin
-
 from pycoin import key
 
 # Popular derivation paths
@@ -16,12 +14,42 @@ class Bip32:
         self.node = key.Key.from_text(extended_key)
 
     def get_xpub(self):
-        return self.node.public_copy()
+        return self.node.as_text()
+
+    def get_xprv(self):
+        if self.node.is_private():
+            return self.node.as_text(as_private=True)
+        else:
+            print("Your Bip32_Node is not derived from an xprv")
 
     def address(self, use_uncompressed=False):
-        """Return the public address representation of this key, if available.
-        If use_uncompressed is not set, the preferred representation is returned."""
+        """Return the public address representation of this key, if available."""
         return self.node.address(use_uncompressed)
+
+    def private_key(self, use_uncompressed=False):
+        """Returns the private key for this extended private key"""
+        if self.node.is_private():
+            return self.node.wif(use_uncompressed)
+        else:
+            print("Your Bip32_Node is not derived from an xprv")
+
+    def get_children(self, derivation_path=DERIVATION_PATH_ELECTRUM_SV, index_start=0, index_end=20):
+        """Specify the start and end indexes for generating xpubs / xprv keys at the desired derivation_path.
+        Default derivation path is m/0' for Handcash and Electrum SV wallets, therefore this is the default
+        xprv --> xprv
+        xpub --> xpub
+
+        :param derivation_path: a path of subkeys denoted by numbers and slashes. Use H or p
+        for private key derivation. End with .pub to force the key
+        public.
+        :type `str`
+        :param index_start: Index to start deriving new children
+        :param index_end: Index to stop deriving new children
+        :rtype children: a `list` of `xprv` or `xpub` bip32 nodes at the derivation path specified """
+        children = []
+        for i in range(index_start, index_end):
+            children.append(self.node.subkey_for_path(derivation_path + '/{}'.format(i)))
+        return children
 
     def get_child_addresses(self, derivation_path=DERIVATION_PATH_ELECTRUM_SV, index_start=0, index_end=20):
         """Specify the start and end indexes for generating addresses at the desired derivation_path.
@@ -35,7 +63,7 @@ class Bip32:
         """wif format = True will output WIF format strings instead of private_key objects
         alternatively the private key objects have a wif() method to generate this later
         for use with bitsv transaction functions"""
-        if self.node.is_private:
+        if self.node.is_private():
             try:
                 keys_list = []
                 for i in range(index_start, index_end):
@@ -49,6 +77,22 @@ class Bip32:
         else:
             print("Your Bip32_Node is not derived from an xprv. xpub keys cannot be used to generate private keys")
 
+    def sign(self, data):
+        """Return a der-encoded signature for a hash h.
+        Will throw a RuntimeError if this key is not a private key"""
+        return self.node.sign(data)
+
+    def verify(self, data, sig):
+        """Return whether a signature is valid for hash h using this key."""
+        return self.node.verify(data, sig)
+
+    def chain_code(self):
+        """32 byte chain code for xpub or xprv"""
+        return self.node.chain_code()
+
+    def fingerprint(self):
+        return self.node.fingerprint()
+
 
 # Old Aliases planned for deprecation
 class Bip32utils:
@@ -57,8 +101,8 @@ class Bip32utils:
     All functions (except for 'get_xprv_bip32_node()' and 'get_xpub_bip32_node()' constructors)
     use bip32 node objects as the input value rather than a string of the xprv or xpub keys"""
     # Functions for generating pycoin.key.BIP32Node objects
-    get_xprv_bip32_node = pycoin.key.Key.from_text
-    get_xpub_bip32_node = pycoin.key.Key.from_text
+    get_xprv_bip32_node = key.Key.from_text
+    get_xpub_bip32_node = key.Key.from_text
 
     @staticmethod
     def xpub_from_xprv(xprv):
