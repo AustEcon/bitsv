2f259efd54b646595f8249e30c8345abe0a1e55d
Gu Lu <gemfeeling@hotmail.com>
2020-02-12 18:32:56 +0800

    the url of bitindex API was changed from 'api.bitindex.network' to 'api.mattercloud.net'

--- bitsv/network/services/bitindex.py
+++ bitsv/network/services/bitindex.py
@@ -16,7 +16,7 @@ class BitIndex:
             'Accept': 'application/json'
         }
 
-        r = requests.get('https://api.bitindex.network/api/v2/addrs/balance?address={}'.format(address),
+        r = requests.get('https://api.mattercloud.net/api/v2/addrs/balance?address={}'.format(address),
                          headers=headers)
         r.raise_for_status()
         return r.json()
@@ -29,7 +29,7 @@ class BitIndex:
             'Accept': 'application/json'
         }
 
-        r = requests.get('https://api.bitindex.network/api/v2/addrs/txs?address={}'.format(address), headers=headers)
+        r = requests.get('https://api.mattercloud.net/api/v2/addrs/txs?address={}'.format(address), headers=headers)
         r.raise_for_status()
         return r.json()
 
@@ -41,7 +41,7 @@ class BitIndex:
             'Accept': 'application/json'
         }
 
-        r = requests.get('https://api.bitindex.network/api/v2/tx/{}'.format(tx), headers=headers)
+        r = requests.get('https://api.mattercloud.net/api/v2/tx/{}'.format(tx), headers=headers)
         r.raise_for_status()
         return r.json()
 
@@ -54,7 +54,7 @@ class BitIndex:
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-        r = requests.post('https://api.bitindex.network/api/addrs/utxo', data=json_payload, headers=headers)
+        r = requests.post('https://api.mattercloud.net/api/addrs/utxo', data=json_payload, headers=headers)
         r.raise_for_status()
         return [Unspent(amount=currency_to_satoshi(tx['amount'], 'bsv'),
                         script=tx['scriptPubKey'],
@@ -71,7 +71,7 @@ class BitIndex:
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-        r = requests.post('https://api.bitindex.network/api/v2/tx/send', data=json_payload, headers=headers)
+        r = requests.post('https://api.mattercloud.net/api/v2/tx/send', data=json_payload, headers=headers)
         r.raise_for_status()
         return r.json()
 
@@ -82,7 +82,7 @@ class BitIndex:
             'Accept': 'application/json',
             'api_key': api_key
         }
-        r = requests.post('https://api.bitindex.network/api/v2/xpub/register?xpub={}'.format(xpub),
+        r = requests.post('https://api.mattercloud.net/api/v2/xpub/register?xpub={}'.format(xpub),
                           headers=headers)
         return r
 
@@ -93,7 +93,7 @@ class BitIndex:
             'Accept': 'application/json',
             'api_key': api_key
         }
-        r = requests.get('https://api.bitindex.network/api/v2/xpub/status?xpub={}'.format(xpub),
+        r = requests.get('https://api.mattercloud.net/api/v2/xpub/status?xpub={}'.format(xpub),
                          headers=headers)
         r.raise_for_status()
         return r.json()
@@ -105,7 +105,7 @@ class BitIndex:
             'Accept': 'application/json',
             'api_key': api_key
         }
-        r = requests.get('https://api.bitindex.network/api/v2/xpub/balance?xpub={}'.format(xpub),
+        r = requests.get('https://api.mattercloud.net/api/v2/xpub/balance?xpub={}'.format(xpub),
                          headers=headers)
         r.raise_for_status()
         return r.json()
@@ -117,7 +117,7 @@ class BitIndex:
             'Accept': 'application/json',
             'api_key': api_key
         }
-        r = requests.get('https://api.bitindex.network/api/v2/xpub/utxos?xpub={}'.format(xpub),
+        r = requests.get('https://api.mattercloud.net/api/v2/xpub/utxos?xpub={}'.format(xpub),
                          headers=headers)
         r.raise_for_status()
         return r.json()
@@ -129,7 +129,7 @@ class BitIndex:
             'Accept': 'application/json',
             'api_key': api_key
         }
-        r = requests.get('https://api.bitindex.network/api/v2/xpub/txs?xpub={}'.format(xpub),
+        r = requests.get('https://api.mattercloud.net/api/v2/xpub/txs?xpub={}'.format(xpub),
                          headers=headers)
         r.raise_for_status()
         return r.json()
